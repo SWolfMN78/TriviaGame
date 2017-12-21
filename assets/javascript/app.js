cea6d9d3644cf2ea7c448ca419fa158b46ec2ba7
Wolfknightx <Wolfknightx@Gmail.com>
2017-12-21 13:58:56 -0600

    nearly complete project

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -78,14 +78,12 @@ var inGameAndInf = [ /*Array to hold all the required information. Once answered
 	// });
 
 	$().ready(function(){
-		//1) base out all fields
-		
 		//load up the questions and answers.
 		questionAndAnswers();
 		//run the timer - reset the questions when timer runs out
 		runClock();
-		//
-
+		//check to see how the player did and log their totals to wins/losses/timeout (draws).
+		winDrawLossCheck();
 		
 
 		// $.each(inGameAndInf, function(i){
@@ -111,12 +109,13 @@ var inGameAndInf = [ /*Array to hold all the required information. Once answered
 		questionItem = inGameAndInf[Math.floor(Math.random()*inGameAndInf.length)];  //gets a random item from the array
 		//check to see if the question has been asked (question asked is true) if so then ask another question.
 		if(questionItem.questionAsked){
-			questionAndAnswers();
+			questionAndAnswers(); 
 			return;
 		}
 
 		gameQuestion = questionItem.question; //item holding the possible answers
 		gameAnswerItm = questionItem.possibleAnswers; //item of the possible answers and push into a solid item
+		gameValidAns = questionItem.goodAnswer //store the correct answer
 
 		$("#iTxtQuestion").text(gameQuestion); //tell the lable to take in this text and display it.
 
@@ -133,10 +132,32 @@ var inGameAndInf = [ /*Array to hold all the required information. Once answered
 		$("#iLblAnswer2").text(gameAnswerItm[1]);
 		$("#iLblAnswer3").text(gameAnswerItm[2]);
 		$("#iLblAnswer4").text(gameAnswerItm[3]);
-
 		 // set the boolean to T = true means it's answers and should not be asked again.
 		 questionItem.questionAsked = true;
+
+		
+
 		};
+
+	function winDrawLossCheck(){
+		 //check each answer for the correct answer or if the timer timed out.
+		$(".cUserAnswer1").click(function(){
+			var userGuess = $(this).text(); //pull the value of what the user guessed out
+			if(userGuess === gameValidAns) { //compare the information of what was guessed to the correct answer.
+				correctAnswers += 1;
+				alert("Nicely done, next question!");
+				questionAndAnswers(); //load the next question
+				gameClock = 31; //put on an extra second so that the clock reads correctly.
+			return;
+			}
+			if (userGuess !== gameValidAns){
+				wrongAnswers += 1; //add one to the loss score
+				alert("That is not the correct answer. You actually want " + gameValidAns + ".");
+				questionAndAnswers(); //load the next question
+				gameClock = 31; //put on an extra second so that the clock reads correctly.
+			}
+		})
+	}
 
 	function runClock(){
 		//ticket to reduce the clock by 1 second.
@@ -146,18 +167,22 @@ var inGameAndInf = [ /*Array to hold all the required information. Once answered
 	function reduceTime(){
 		gameClock --; //removes 1 from the time
 
+		//the clock display
 		$("#iLblTimer").html(gameClock);
 
+		//if the clock reaches 0 then...
 		if (gameClock === 0) {
 			alert("Sorry your time is up, on to the next question");
-			stopClock();
+			timedOut += 1; //add one to the total times that the clock timed out.  This will be displayed at the end.
+			stopClock(); //stop the clock from running
+			//reset the gameboard:
 			questionAndAnswers();
 			gameClock = 31;
 			runClock();
 		}
 	}
 
-	function stopClock() {
+	function stopClock(){
 		//stop the timer and reset the clock
 		clearInterval(clockIntervalID);
 	}
